ac31c511d7326fb73604154681d27264b22fa5f1
ciper <anmolkarki101@gmail.com>
2023-11-27 09:54:47 +0545

    Implemented Google scraping in Go: dynamic URL construction, user agent randomization. Work in progress - fetching search results next!

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "crypto/rand"
+import (
+	"crypto/rand"
+	"fmt"
+	"strings"
+)
 
-var googleDomains = map[string]string{}
+var googleDomains = map[string]string{
+	"com": "https://google.com/search?q=",
+	"za":  "https://www.google.co.za/search?q=",
+}
 
 type SearchResult struct {
 	ResultRank  int
@@ -18,7 +25,29 @@ func randomUserAgent() string {
 	randNum := rand.Int() % len(userAgents)
 	return userAgents[randNum]
 }
+func buildGoogleUrls(searchTerm, countryCode, pages, count int) {
+	toScrape := []string{}
+	searchTerm = strings.Trim(searchTerm, " ")
+	searchTerm = strings.Replace(searchTerm, " ", "+", -1)
+	if googleBase, found := googleDomains[countryCode]; found {
+		for i := 0; i < pages; i++ {
+			start := i * count
+			scrapeURL := fmt.Sprintf(googleBase, searchTerm, count, languageCode, start)
 
+		}
+	}
+
+}
+func GoogleScrape(searchTerm, countryCode, languageCode string, pages, count) ([]SearchResult, error) {
+	results := []SearchResult{}
+	resultCounter := 0
+	googlePages, err := buildGoogleUrls(searchTerm, countryCode,languageCode, pages, count)
+}
 func main() {
-
+	res, err := GoogleScrape("Andrew Huberman", "en", "com", 1, 30)
+	if err == nil {
+		for _, res := range res {
+			fmt.Println(res)
+		}
+	}
 }
